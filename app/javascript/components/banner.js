2bcbff02eeeac83612812041af93efefbcb1079a
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-07 20:21:56 +0900

    style dose and cocktail creation pages

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Try one of our delicious cocktails", "Reach a heightened state of mind"],
+    strings: ["Try one of our delicious cocktails", "Reach heightened states of mind"],
     typeSpeed: 50,
     loop: true
   });
