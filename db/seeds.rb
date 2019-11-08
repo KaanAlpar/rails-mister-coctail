f18b9e376467c2bc37a2382e26149896293b98fe
Kaan Alpar <kaanalpar33@gmail.com>
2019-11-08 11:32:15 +0900

    Uncomment seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,20 @@ require 'open-uri'
 require 'json'
 require 'faker'
 
-# buffer = open('http://thecocktaildb.com/api/json/v1/1/list.php?i=list').read
-# result = JSON.parse(buffer)
-# puts 'Creating ingredients...'
-# result['drinks'].each do |ing_hash|
-#   ing = Ingredient.new(name: ing_hash['strIngredient1'])
-#   p ing
-#   ing.save!
-# end
-# puts 'Finished creating ingredients...'
-
-puts "Creating coctails..."
-50.times do
-  cocktail = Cocktail.new(name: Faker::Beer.brand)
-  cocktail.save!
-  p cocktail
+buffer = open('http://thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+result = JSON.parse(buffer)
+puts 'Creating ingredients...'
+result['drinks'].each do |ing_hash|
+  ing = Ingredient.new(name: ing_hash['strIngredient1'])
+  p ing
+  ing.save!
 end
-puts "Finished creating cocktail..."
+puts 'Finished creating ingredients...'
+
+# puts "Creating coctails..."
+# 50.times do
+#   cocktail = Cocktail.new(name: Faker::Beer.brand)
+#   cocktail.save!
+#   p cocktail
+# end
+# puts "Finished creating cocktail..."
